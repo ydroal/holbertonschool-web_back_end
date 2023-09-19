8fcb04c9efdd3b964b9aaf78ab02bb004580f8dd
ydroal <droal.yoko@gmail.com>
2023-09-19 15:50:53 +0200

    add: task0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Holbertonschool-web_back_end
+
+This is the project of web back-end.
+
+## Repo
+* GitHub repository: holbertonschool-back_front_end
+
---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module to the addition of two numbers
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    calculates the sum of two numbers.
+
+    Args:
+    a (float): Number to calculate
+    b (float): Number to calculate
+
+    Returns:
+    float: their sum as a float.
+    '''
+
+    return a + b
