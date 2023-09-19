e5e1aecb91c729411c0a76b9a7a38ed9429c9adb
ydroal <droal.yoko@gmail.com>
2023-09-19 16:02:03 +0200

    add: task1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module to concatenate two strings
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    concatenate two strings.
+
+    Args:
+    str1 (str): string to concatenate
+    str2 (str): string to concatenate
+
+    Returns:
+    str: concatenated string.
+    '''
+
+    return str1 + str2
