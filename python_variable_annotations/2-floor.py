7eb33e60e1baad17524cf2f45703363301c716a4
ydroal <droal.yoko@gmail.com>
2023-09-20 10:33:30 +0200

    add: task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module to returns the floor of the float.
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    returns the floor of the float.
+
+    Args:
+    n (float): given float number 
+
+    Returns:
+    str: floor of the float.
+    '''
+
+    return math.floor(n)
