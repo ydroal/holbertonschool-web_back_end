aebc85a51a46e6214301f344b3c57df5b7d289dc
ydroal <droal.yoko@gmail.com>
2023-09-20 10:58:43 +0200

    add: task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Module to represent float to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    represent float to string.
+
+    Args:
+    n (float): given float number
+
+    Returns:
+    str: string representation of the float.
+    '''
+
+    return str(n)
