40aa2152c3bfb9071297d20b8284cfba9b9db6cf
ydroal <droal.yoko@gmail.com>
2023-09-22 11:51:02 +0200

    add: task0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+Module that contains the asynchronous function wait_random
+'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    '''
+    Waits for a random delay between 0 and max_delay seconds and returns it.
+
+    Args:
+    max_delay (int): The maximum delay value. Defaults is 10
+
+    Returns:
+        float: The delay value.
+    '''
+
+    random_number = random.uniform(0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
