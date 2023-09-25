7b7a128e9b81bb9fc17c7b87943cc0fbe705507d
ydroal <droal.yoko@gmail.com>
2023-09-25 10:47:23 +0200

    add: task0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Module that yield a random number between 0 and 10 asynchronously
+'''
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    '''
+    Asynchronously yield random numbers.
+
+    This coroutine will loop 10 times. Each iteration, it will
+    wait for 1 second and then yield a random number between 0 and 10.
+
+    Yields:
+        float: A random number between 0 and 10.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
