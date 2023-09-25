2ad23a1d3728c7e52710a6f3d2a76d8f0f6f033b
ydroal <droal.yoko@gmail.com>
2023-09-25 11:27:26 +0200

    fix: type annotation for function with Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Module that yield a random number between 0 and 10 asynchronously
 '''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''
     Asynchronously yield random numbers.
 
