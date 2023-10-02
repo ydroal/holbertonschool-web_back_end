ab1b2c8af02572a9c16031b3454dc0cb5b87fac0
ydroal <droal.yoko@gmail.com>
2023-10-02 17:17:09 +0200

    update: add type hints

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,10 +2,12 @@
 '''
 Module to returns the log message obfuscated:
 '''
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     '''
     returns the log message obfuscated
 
