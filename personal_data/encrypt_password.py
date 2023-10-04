317ba93b5e60dc429254e2d2cf251f446471368c
ydroal <droal.yoko@gmail.com>
2023-10-04 21:58:44 +0000

    add: task5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''
+    returns a salted, hashed password, which is a byte string.
+
+    Args:
+        password (str): password to be hashed
+
+    Returns:
+        bytes: the hashed password
+    '''
+    salt = bcrypt.gensalt()
+
+    hashed = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed
