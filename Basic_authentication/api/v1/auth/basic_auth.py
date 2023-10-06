8e32ac4bfc1fe7f39acaf9dce7aac00b36470d12
ydroal <droal.yoko@gmail.com>
2023-10-06 13:57:18 +0000

    update: adddecode_base64_authorization_header()

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module to manage the API authentication
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -25,3 +26,25 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''
+        Args:
+        base64_authorization_header (str): Base64 part of the Auth header
+
+        Returns:
+        str: decoded value of a Base64 of base64_authorization_header
+        '''
+
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        # Base64のデコード時に無効なBase64文字列が渡されるとbinascii.Errorを発生させる
+        except Exception:
+            return None
