0749f3939125e28b9d8b683af4be8be2ced2aa84
ydroal <droal.yoko@gmail.com>
2023-10-09 14:36:23 +0000

    fix: add type hint

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Returns a User object
         """
