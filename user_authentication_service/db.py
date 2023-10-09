5eebc7b335de6dcd7e08f46fd886c79d93755f05
ydroal <droal.yoko@gmail.com>
2023-10-09 21:19:18 +0000

    add: task2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base
 from user import User
 
@@ -39,3 +41,16 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Searches for the first user in the database
+        that matches the provided key and value
+        """
+        try:
+            found_user = self._session.query(User).filter_by(**kwargs).first()
+            if not found_user:
+                raise NoResultFound
+        except InvalidRequestError:
+            raise
+        return found_user
