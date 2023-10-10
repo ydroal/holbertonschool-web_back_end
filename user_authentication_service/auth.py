1656a55f35ffacc24aa706e6578cdf5af3db648a
ydroal <droal.yoko@gmail.com>
2023-10-10 10:19:57 +0000

    update: move _hash_password to standalone function

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,26 +7,27 @@ from db import DB
 from user import User
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Generate a salted hash of the input password using bcrypt
+
+    Args:
+    password (str): The password to hash
+
+    Returns (byte): The hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Generate a salted hash of the input password using bcrypt
-
-        Args:
-        password (str): The password to hash
-
-        Returns (byte): The hashed password
-        """
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode(), salt)
-
-        return hashed
 
     def register_user(self, email: str, password: str) -> User:
         """
@@ -42,7 +43,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists.')
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
 
             return new_user
