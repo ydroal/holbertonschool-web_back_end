ce48e055ebdbc8f44caf0cc7dd576a8fb37f8202
ydroal <droal.yoko@gmail.com>
2023-10-12 14:55:26 +0000

    add: task16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -127,3 +127,23 @@ class Auth:
         None
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate a reset token and store it in DB
+
+        Args:
+        email (str): user's email
+
+        Returns (str):
+        token is generated
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except (NoResultFound, InvalidRequestError):
+            raise ValueError
+
+        uuid = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=uuid)
+        return user.reset_token
