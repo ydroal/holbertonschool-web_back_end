ad5486baf6d3faff4a2d216c60d91de2f56dd5e5
ydroal <droal.yoko@gmail.com>
2023-10-17 21:11:34 +0200

    add: task1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,9 +16,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    
     def test_access_nested_map(self, nested_map, path, expected_output):
         """
         Test access_nested_map method
         """
-        self.assertEqual(utils.access_nested_map(nested_map, path), expected_output)
+        self.assertEqual(utils.access_nested_map(nested_map, path),
+                         expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test access_nested_map method - case exception
+        """
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
