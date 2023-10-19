efe4ad2850ae75a371efd7104f5c103bb07da585
ydroal <droal.yoko@gmail.com>
2023-10-19 23:11:25 +0200

    add: task5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,49 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_class._public_repos_url,
                              "https://api.github.com/orgs/google/repos")
             mock_org.assert_called_once()
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+        Test public_repos method
+        """
+        mock_payload = [
+            {
+                "name": "episodes.dart",
+                "license": {
+                    "key": "bsd-3-clause",
+                    "name": "BSD 3-Clause \"New\" or \"Revised\" License",
+                    "spdx_id": "BSD-3-Clause",
+                    "url": "https://api.github.com/licenses/bsd-3-clause",
+                    "node_id": "MDc6TGljZW5zZTU="
+                },
+            },
+            {
+                "name": "cpp-netlib",
+                "license": {
+                    "key": "bsl-1.0",
+                    "name": "Boost Software License 1.0",
+                    "spdx_id": "BSL-1.0",
+                    "url": "https://api.github.com/licenses/bsl-1.0",
+                    "node_id": "MDc6TGljZW5zZTI4"
+                },
+            }
+        ]
+        mock_get_json.return_value = mock_payload
+
+        test_class = GithubOrgClient("google")
+
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_url:
+
+            mock_url.return_value = "https://api.github.com/orgs/google/repos"
+            self.assertEqual(test_class.public_repos(
+                license="bsd-3-clause"), ["episodes.dart"])
+
+            self.assertEqual(test_class.public_repos(
+                license="bsl-1.0"), ["cpp-netlib"])
+
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/google/repos")
+
+            mock_url.assert_called_once()
