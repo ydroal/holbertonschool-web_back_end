8f9abb26e491451ba3fde2cf28c4673d5dd940d5
ydroal <droal.yoko@gmail.com>
2023-10-19 23:42:15 +0200

    add: task7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -93,3 +93,14 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/google/repos")
 
             mock_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected_output):
+        """
+        Test has_license method
+        """
+        self.assertEqual(GithubOrgClient.has_license(
+            repo, license_key), expected_output)
