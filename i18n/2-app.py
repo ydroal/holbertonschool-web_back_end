ee7412b5b6c970841b36ffad6b58a9704b9f877f
ydroal <droal.yoko@gmail.com>
2023-10-20 23:44:44 +0200

    add: task2

---
+++ i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """
+    Define Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 2-index.html
+    """
+    return render_template('2-index.html')
