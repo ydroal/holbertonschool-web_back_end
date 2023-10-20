f137f8ba58c3ea0509267b044a99298367bd9401
ydroal <droal.yoko@gmail.com>
2023-10-20 23:28:34 +0200

    add: task1

---
+++ i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Define Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 1-index.html
+    """
+    return render_template('1-index.html')
