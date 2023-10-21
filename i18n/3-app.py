4e27be09931878020b154da5064f4742011562da
ydroal <droal.yoko@gmail.com>
2023-10-21 22:58:23 +0200

    fix: Add @babel.localeselector decorator and update config file for Jinja2 environment setup

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -16,16 +16,17 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
 def get_locale():
     """
     Get locale from request
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
@@ -37,5 +38,5 @@ def index():
     return render_template('3-index.html')
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
