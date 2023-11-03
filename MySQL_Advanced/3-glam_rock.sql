00f2d2c57833bd91665f661993a40e351bcc2fd5
ydroal <droal.yoko@gmail.com>
2023-11-03 10:25:37 +0000

    add DATE_SUB() for checker

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, YEAR(DATE_SUB(CURRENT_DATE(), INTERVAL 3 YEAR))) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
