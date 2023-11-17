21b583f79d57e77c63dca22a22b7d199deb7fec1
ydroal <droal.yoko@gmail.com>
2023-11-17 14:51:00 +0000

    add: task4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') throw new TypeError('Code must be a number');
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) throw new TypeError('Currency must be a Currency object');
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
