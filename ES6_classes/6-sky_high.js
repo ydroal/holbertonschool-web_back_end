a836d53a5e74bb4a097a4ab8cd48c58174c0acc5
ydroal <droal.yoko@gmail.com>
2023-11-17 16:15:47 +0000

    add: task6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  /* eslint-disable */
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
