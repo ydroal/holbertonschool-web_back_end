c697878f90be6a2ebfe4efaa58e1d4238a8fe233
ydroal <droal.yoko@gmail.com>
2023-11-17 10:44:40 +0000

    add: task1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const room = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  const res = [room, room2, room3];
+  return res;
+}
