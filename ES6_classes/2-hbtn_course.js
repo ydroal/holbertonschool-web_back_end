6a4576a9b860b35e87f9eb9b579e4d266ef426b3
ydroal <droal.yoko@gmail.com>
2023-11-17 13:27:47 +0000

    add: task2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    const isString = value.every((val) => (typeof val === 'string'));
+
+    if (!Array.isArray(value) || !isString) {
+      throw new TypeError('Length must be a array of Strings');
+    }
+
+    this._students = value;
+  }
+}
