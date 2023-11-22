7fe5ef3dbf6665cd6a53ce4c17165e41511407c9
ydroal <droal.yoko@gmail.com>
2023-11-22 18:10:23 +0000

    add: task5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (value < -128 || value > 127) {
+    throw new RangeError('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  view[position] = value;
+
+  return new DataView(buffer);
+}
