45b43e75fbff767d9aef8724b4947ecafc10f0bc
ydroal <droal.yoko@gmail.com>
2023-11-23 09:17:44 +0000

    fix: add throw

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,6 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   if (value < -128 || value > 127) {
     throw new RangeError('Position outside range');
   }
+  if (position > length) {
+    throw new Error('Position outside range');
+  }
 
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
