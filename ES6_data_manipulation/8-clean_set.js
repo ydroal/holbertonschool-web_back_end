bb82cee3ad526b9cde45e5f464591a865ff3dc17
ydroal <droal.yoko@gmail.com>
2023-11-23 14:15:13 +0000

    add: task8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  let newStr = '';
+  for (let str of set) {
+    if (str.startsWith(startString)) {
+      str = `${str.replace(startString, '')}-`;
+      newStr = newStr.concat(str);
+    }
+  }
+  const lastChar = newStr.slice(-1);
+  if (lastChar === '-') newStr = newStr.slice(0, -1);
+
+  return newStr;
+}
