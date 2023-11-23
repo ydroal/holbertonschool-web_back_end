267b1bbed24f2bfc51df94a6782a72718e825f71
ydroal <droal.yoko@gmail.com>
2023-11-23 15:29:58 +0000

    add: task9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const res = new Map();
+  res.set('Apples', 10);
+  res.set('Tomatoes', 10);
+  res.set('Pasta', 1);
+  res.set('Rice', 1);
+  res.set('Banana', 5);
+
+  return res;
+}
