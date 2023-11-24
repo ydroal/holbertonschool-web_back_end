42beb04f601cb43ed7c7e2c470a5eba82cd9cf51
ydroal <droal.yoko@gmail.com>
2023-11-24 13:58:32 +0000

    add: task1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,24 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+// キーの入力待ち状態にする。
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+// 標準入力終了時のイベント処理
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
+// Ctrl + C が入力された場合のイベント処理
+process.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  // SIGINTイベントでは、プログラムを明示的に終了させるためにprocess.exit(0)を呼び出す
+  process.exit(0);
+});
+
+// 入力された１行を読み込んだ時のイベント処理
+process.stdin.on('data', (inputData) => {
+  // 末尾の改行を取り除く。
+  const input = inputData.trim();
+  console.log(`Your name is: ${input}`);
+});
