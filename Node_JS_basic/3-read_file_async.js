b9974666b354ca1c26baaf275eafb0e513875470
ydroal <droal.yoko@gmail.com>
2023-11-29 11:57:02 +0000

    add: task3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const fsp = require('fs').promises;
+
+async function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const text = await fsp.readFile(path, 'utf8');
+  const data = text.split('\n');
+  const students = data.slice(1).filter((line) => line.length > 0);
+  console.log(`Number of students: ${students.length}`);
+
+  const group = students.reduce((result, line) => {
+    const [firstname, , , field] = line.split(',');
+
+    const element = result.find((s) => s.field === field);
+    if (element) {
+      element.count += 1;
+      element.firstnames.push(firstname);
+    } else {
+      result.push({
+        field,
+        count: 1,
+        firstnames: [firstname],
+      });
+    }
+    return result;
+  }, []);
+
+  group.forEach((element) => {
+    console.log(`Number of students in ${element.field}: ${element.count}. List: ${element.firstnames.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
