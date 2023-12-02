9df34faa3e6137cc63a8838162da94b474ab1fca
ydroal <droal.yoko@gmail.com>
2023-12-02 14:36:20 +0000

    add: task3

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Validates if Utils function is called', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    
+    spy.restore();
+  });
+});
+
---
+++ unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  const aRounded = Math.round(a);
+  const bRounded = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return aRounded + bRounded;
+    case 'SUBTRACT':
+      return aRounded - bRounded;
+    case 'DIVIDE':
+      return bRounded === 0 ? 'Error' : aRounded / bRounded;
+  }
+}
+
+const Utils = {
+  calculateNumber
+};
+
+module.exports = Utils;
