3717243e082dcf8ad783e53211510f4ba46937e2
ydroal <droal.yoko@gmail.com>
2023-12-03 13:32:13 +0000

    fix: add test code to root and change small regex to set endpoint

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get('/cart/:id([0-9]+)', (req, res) => {
   const id = req.params.id;
   res.send(`Payment methods for cart ${id}`);
 });
--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,6 +1,22 @@
 const expect = require('chai').expect;
 const request = require('request');
 
+describe('get request to root', () => {
+  it('returns status code 200', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct message', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
 describe('get request to cart page with parameter', () => {
   it('returns status code 200', (done) => {
     request.get('http://localhost:7865/cart/12', (error, response, body) => {
@@ -22,6 +38,5 @@ describe('get request to cart page with parameter', () => {
       done();
     });
   });
-
 });
 
