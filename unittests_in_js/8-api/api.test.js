4ca9a33b77d361288c58a73ad383cea34bf9c308
ydroal <droal.yoko@gmail.com>
2023-12-03 00:12:16 +0000

    add: task8

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('get request to root', () => {
+  it('returns status code 200', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct message', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+});
