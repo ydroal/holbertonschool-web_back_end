6cd760db222cb129e0dca3d3e4c40ca6b7787c03
ydroal <droal.yoko@gmail.com>
2023-12-03 12:27:34 +0000

    add: task9

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('get request to cart page with parameter', () => {
+  it('returns status code 200', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct message', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('returns status code 404', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+});
+
