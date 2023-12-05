dd8522a243089f6104cd34a04af4f175d4e79328
ydroal <droal.yoko@gmail.com>
2023-12-05 23:27:26 +0000

    add: task2, downgrade node-redis for task2

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) console.log('Error:', err);
+    else console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
