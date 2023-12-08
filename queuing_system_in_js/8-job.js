345dcc50c90da2d3022504c53834f953178ec91d
ydroal <droal.yoko@gmail.com>
2023-12-08 14:51:29 +0000

    add: task8

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function createPushNotificationsJobs (jobs, queue) {
+  if (!Array.isArray(jobs)) throw new Error('Jobs is not an array');
+  
+  jobs.forEach(element => {
+    const job = queue.create('push_notification_code_3', element).save(err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Notification job created: ${job.id}`);
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+
+    }).on('failed', error => {
+      console.log(`Notification job ${job.id} failed`, error);
+
+    }).on('progress', progress => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
